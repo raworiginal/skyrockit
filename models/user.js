b4fd3ea04e59ae0451a35c8ef9f67b66fda863bb
raworiginal <randall.a.wolfe@gmail.com>
2025-07-23 10:08:51 -0700

    defined embedded aplication schema

--- models/user.js
+++ models/user.js
@@ -1,14 +1,38 @@
 const mongoose = require("mongoose");
 
+const applicationSchema = new mongoose.Schema({
+  // properties of the applications
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["interested", "applied", "interviewing", "rejected", "accepted"],
+    required: true,
+  },
+});
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model("User", userSchema);
