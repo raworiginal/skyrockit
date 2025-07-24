1590e409ed1ea4ff3d37f6ddc1542c8e7ea545c7
raworiginal <randall.a.wolfe@gmail.com>
2025-07-24 10:29:59 -0700

    complete new application functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -5,7 +5,10 @@ const User = require("../models/user.js");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("applications/index.ejs");
+    const currentUser = await User.findById(req.session.user._id);
+    res.render("applications/index.ejs", {
+      applications: currentUser.applications,
+    });
   } catch (error) {
     console.log(error);
     res.redirect("/");
@@ -14,6 +17,18 @@ router.get("/", async (req, res) => {
 
 router.get("/new", async (req, res) => {
   res.render("applications/new.ejs");
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.applications.push(req.body);
+    await currentUser.save();
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
