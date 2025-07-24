442069a8e5bf544f78ffdbdc575d7af94f5099e0
raworiginal <randall.a.wolfe@gmail.com>
2025-07-24 13:26:47 -0700

    completed edit page

--- controllers/applications.js
+++ controllers/applications.js
@@ -47,6 +47,19 @@ router.get("/:applicationId", async (req, res) => {
   }
 });
 
+router.get("/:applicationId/edit", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render("applications/edit.ejs", {
+      application: application,
+    });
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
+});
+
 router.delete("/:applicationId", async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
